089836297d7e6bdbc7719e5305969b599f5a5c74
denis-50 <shafwansal9105@gmail.com>
2023-09-25 21:44:57 +0700

    add test.html

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let biodata = [
+  ["tommy", 26, "jakarta"],
+  ["danis", 40, "depok"],
+  ["rajas", 17, "padang"],
+];
+
+for (let i = 0; i < biodata.length; i++) {
+  consol;
+  for (let j = 0; j < biodata[i].length; j++) {
+    console.log(biodata[i][j]);
+  }
+}
